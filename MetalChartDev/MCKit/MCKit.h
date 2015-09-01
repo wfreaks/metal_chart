3501946afd87e8bcf176a0acf51c0b07d385ef1c
Mori Keisuke <kei@Keis-MacBookPro.local>
2015-09-02 01:16:41 +0900

    create framework target

---
+++ MetalChartDev/MCKit/MCKit.h
@@ -0,0 +1,19 @@
+//
+//  MCKit.h
+//  MCKit
+//
+//  Created by Mori Keisuke on 2015/09/02.
+//  Copyright © 2015年 freaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCKit.
+FOUNDATION_EXPORT double MCKitVersionNumber;
+
+//! Project version string for MCKit.
+FOUNDATION_EXPORT const unsigned char MCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCKit/PublicHeader.h>
+
+
