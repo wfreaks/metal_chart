6172ab45fbf524692c796dec7970715a82538aac
mori <mori@d-side.co.jp>
2016-04-21 17:42:29 +0900

    [clean] removed unneccessary typedefs.

--- MetalChartDev/FMChart/Engine/Base/base_shared.h
+++ MetalChartDev/FMChart/Engine/Base/base_shared.h
@@ -17,27 +17,27 @@ using namespace simd;
 
 #endif
 
-struct vertex_float2 {
+typedef struct {
 	vector_float2 position;
-};
+} vertex_float2;
 
-struct vertex_index {
+typedef struct {
 	uint32_t index;
-};
+} vertex_index;
 
 // 主に属性付きの値(色などの設定を変える)を表示する際に使う.
 // 別々のパラメータとしてもよいが、数の一致や扱い方の煩雑さなどからこちらの方が使い易いと思う.
-struct indexed_float {
+typedef struct {
 	float value;
 	uint32_t idx;
-};
+} indexed_float;
 
-struct indexed_float2 {
+typedef struct {
 	vector_float2 value;
 	uint32_t idx;
-};
+} indexed_float2;
 
-struct uniform_projection_cart2d {
+typedef struct {
 	vector_float2 origin;
 	vector_float2 value_scale;
 	vector_float2 value_offset;
@@ -45,13 +45,13 @@ struct uniform_projection_cart2d {
 	vector_float2 physical_size;
 	vector_float4 rect_padding;
 	float  screen_scale;
-};
+} uniform_projection_cart2d;
 
-struct uniform_series_info {
+typedef struct {
 	uint32_t vertex_capacity;
 	uint32_t index_capacity;
 	uint32_t offset;
-};
+} uniform_series_info;
 
 // 角度表現はすべてradianで統一する.
 // r軸のスケールは、基本が1dip($(scale) pixels)が1に相当、そこにradius_scaleをかけたものとする.
@@ -65,21 +65,6 @@ typedef struct uniform_projection_polar {
 	vector_float4 rect_padding;
 	float screen_scale;
 } uniform_projection_polar;
-
-
-#ifdef __OBJC__
-
-typedef struct vertex_float2 vertex_float2;
-typedef struct vertex_index vertex_index;
-typedef struct uniform_series_info uniform_series_info;
-
-typedef struct uniform_projection_cart2d uniform_projection_cart2d;
-typedef struct uniform_projection_polar uniform_projection_polar;
-
-typedef struct indexed_float indexed_float;
-typedef struct indexed_float2 indexed_float2;
-
-#endif
 
 
 #endif /* base_shared_h */
